351c8e238a2f5f24d7df5568fc9b9d2c34810bf9
bqw_5189 <bqw_5189@163.com>
2015-05-08 15:35:13 +0800

    build ios

--- platforms/android/assets/www/modules/menu/controllers.js
+++ platforms/android/assets/www/modules/menu/controllers.js
@@ -1,16 +1,22 @@
 angular.module('menu.controllers', [])
 
 .controller('MenuCtrl', function($scope) {
+	$scope.items = [{title:'腊肉炒饭',vid:'XOTQ3ODk5ODE2'},{title:'锅包肉',vid:'XODY4Nzg3MjQw'}]
+	})
+	
+.controller('CookingCtrl', function($scope,$stateParams) {
+	
+	
 	player = new YKU.Player('youkuplayer',{
 		
 		styleid: '0',
 		
 		client_id: '0e7361bc660490f3',
 		
-		vid: 'XOTQ3ODk5ODE2',
+		vid: $stateParams.vid,
 		
 		embsig: 'ce8485584e2c92e3839544e4e1e470b9'
 		
-		});
-})
-;
+		});	
+	});
+
--- platforms/android/assets/www/modules/menu/views.js
+++ platforms/android/assets/www/modules/menu/views.js
@@ -6,10 +6,19 @@ fire.config(function($stateProvider, $urlRouterProvider) {
     controller:'MenuCtrl'
   }) 
   .state('fire.menu.cooking', {
-	    url: "/cooking",
+	    url: "/cookings/:vid",
 	    views: {
 		      'menuContent': {
 		  	    templateUrl: "modules/menu/cooking.html",
+			    controller:'CookingCtrl'
+		      }
+		    }
+	  })
+	.state('fire.menu.cookings', {
+	    url: "/cookings",
+	    views: {
+		      'menuContent': {
+		  	    templateUrl: "modules/menu/cookings.html",
 			    controller:'MenuCtrl'
 		      }
 		    }
